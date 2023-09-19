b9593db2536b09e3de9c888ce4f2fdf4d586dd84
qaruda <70809588+francistinao@users.noreply.github.com>
2023-09-19 16:28:35 +0800

    added validators

--- api/controllers/login.ts
+++ api/controllers/login.ts
@@ -1,23 +1,53 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
 import { db } from "../database/connect.ts";
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
-export const login = async (
-	req: Request,
-	res: Response,
-	next: NextFunction
-) => {
-	const { stud_id } = req.body;
-	console.log(stud_id);
+type Payload = {
+	id: string;
+	student_id: string;
+	email: string;
+	iat: number;
+};
+
+export const login = async (req: Request, res: Response) => {
+	const { stud_id, password } = req.body;
+
 	try {
-		const student_admin = await db.query(
-			`SELECT * FROM student WHERE student_id = "${stud_id}"`
+		const student = await db.query(
+			`SELECT student_id, password FROM student WHERE student_id = '${stud_id}'`
 		);
 
-		if (!student_admin) {
+		if (!student) {
 			return res.status(400).json({ message: "Student not found!" });
 		}
 
-		next();
+		const isMatch = await bcrypt.compare(password, student.rows[0].password);
+
+		if (!isMatch) {
+			return res.status(400).json({ message: "Invalid credentials!" });
+		}
+
+		const payload: Payload = {
+			id: uuid(),
+			student_id: student.rows[0].student_id,
+			email: student.rows[0].email,
+			iat: new Date().getTime(),
+		};
+
+		res.cookie("student_id", payload.student_id, {
+			httpOnly: true,
+			secure: true,
+			sameSite: "none",
+		});
+
+		res
+			.status(200)
+			.json({
+				student,
+				payload,
+				message: "Login successfuly. Proceeding on validation",
+			});
 	} catch (err) {
 		console.log(err);
 		res.status(500).json({ message: "Internal Server Error" });
---
+++ api/controllers/register.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { db } from "../database/connect.ts";
+import bcrypt from "bcrypt";
+
+export const register = async (req: Request, res: Response) => {
+	const {
+		student_id,
+		password,
+		first_name,
+		middle_name,
+		last_name,
+		email,
+		program,
+		year,
+		balance,
+		role,
+	} = req.body;
+
+	try {
+		const student = await db.query(
+			`SELECT * FROM student WHERE student_id = "${student_id}"`
+		);
+
+		if (student) {
+			return res.status(400).json({ message: "Student already exists!" });
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+		const newStudent = await db.query(
+			`INSERT INTO student (student_id, password, first_name, middle_name, last_name, email, program, year, balance, role)
+   VALUES ('${student_id}', '${hashedPassword}','${first_name}', '${middle_name}', '${last_name}', '${email}', '${program}', '${year}', '${balance}', '${role}')`
+		);
+
+		console.log(newStudent);
+
+		res.status(200).json({ message: "Student registered successfuly" });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
+};
